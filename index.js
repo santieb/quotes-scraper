d5af922fe3e2e1562115440eb256eb250b0f49fe
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2023-06-18 15:07:03 -0300

    add: all data from page

--- index.js
+++ index.js
@@ -1,112 +1,60 @@
-import puppeteer from 'puppeteer'
-import { writeExcel, writeJson } from './utils/index.js'
-import randomUseragent from 'random-useragent'
+import puppeteer from "puppeteer";
+import { writeExcel, writeJson } from "./utils/index.js";
+import randomUseragent from "random-useragent";
 
-const openWebPage = async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    slowMo: 400
-  })
-  const page = await browser.newPage()
-
-  await page.goto('https://example.com')
-  await browser.close()
-}
-
-const captureScreenshoot = async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    slowMo: 400
-  })
-  const page = await browser.newPage()
-
-  await page.goto('https://www.mercadolibre.com.ar/')
-  await page.screenshot({ path: 'example.png'})
-  await browser.close()
-}
-
-const navigateWebPage = async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    slowMo: 200,
-
-  })
-  const page = await browser.newPage()
-
-  await page.goto('https://quotes.toscrape.com/')
-  await page.click('a[href="/login"]')
-  await new Promise(resolve => setTimeout(resolve, 3000))
-
-  await browser.close()
-}
-
-const getDataFromWebPage = async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    slowMo: 200,
-  })
-  const page = await browser.newPage()
-
-  await page.goto('https://example.com/')
-
-  const result = await page.evaluate(() => {
-    const title = document.querySelector('h1').innerText
-    const description = document.querySelector('p').innerText
-    const more = document.querySelector('a').innerText
-
-    return { title, description, more }
-  })
-  
-  console.log(result)
-  await browser.close()
-}
-
-const MAX_PAGES = 5
-
-const handleDynamicWebPage = async () => {
+const main = async () => {
   try {
     const browser = await puppeteer.launch({
       headless: false,
-      slowMo: 100,
-    })
+      // slowMo: 100,
+    });
 
-    const page = await browser.newPage()
+    const page = await browser.newPage();
     const userAgent = randomUseragent.getRandom();
     await page.setUserAgent(userAgent);
-    await page.setViewport({ width: 1920, height: 1080})
+    await page.setViewport({ width: 1920, height: 1080 });
 
-    const pages = ['http://quotes.toscrape.com/page/8/', 'http://quotes.toscrape.com/page/9/']
-    const dataa = []
+    await page.goto("http://quotes.toscrape.com");
 
-    for (const url of pages) {
-      await page.goto(url)
-
+    const data = [];
+    let hasNextData = true;
+    while (hasNextData) {
       const result = await page.evaluate(() => {
-        const quotes = document.querySelectorAll('.quote')
-        const data = [...quotes].map((quote, index) => {
-          const quoteText = quote.querySelector('.text').innerText
-          const author = quote.querySelector('.author').innerText
-          const tags = [...quote.querySelectorAll('.tag')].map(tag => tag.innerText)
-  
-          return { id: index, quoteText, author, tags }
-        })
-  
-        return data
-      })
-  
-      const getUrlNextPage = await page.$('.next a')
-      const nextUrl = await page.evaluate(getUrlNextPage => getUrlNextPage.getAttribute('href'), getUrlNextPage)
+        const quotes = document.querySelectorAll(".quote");
+        if (!quotes) return 
 
-      dataa.push(result)
+        const pageData = [...quotes].map((quote, index) => {
+          const quoteText = quote.querySelector(".text").innerText;
+          const author = quote.querySelector(".author").innerText;
+          const tags = [...quote.querySelectorAll(".tag")].map((tag) =>
+            tag.innerText
+          );
+
+          return { quoteText, author, tags };
+        });
+        return pageData
+      });
+
+      data.push(...result)
+
+      const hasNextButton = await page.$('li.next > a');
+      if (!hasNextButton) {
+        hasNextData = false;
+        break
+      }
+      await page.click(`li.next > a`);
     }
 
-    console.log(dataa)
-    
-    //await page.screenshot({ path: './data/screens/example.png'})
-    await browser.close()
-  } catch (err) {
-    console.log(err)
-  }
-}
+    const dataWithId = data.map((quote, index) => ({ id: index + 1, ...quote }))
 
-handleDynamicWebPage()
+    await page.screenshot({ path: './data/screens/example.png'})
+    await browser.close();
+
+    await writeExcel(dataWithId)
+    await writeJson(dataWithId)
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+main();
